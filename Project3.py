4d9dfe719f7e6d3b7546594430d625b31e98a84a
Alex <barajasr@sonoma.edu>
2020-04-28 02:35:57 -0700

    task1a done

--- Project3.py
+++ Project3.py
@@ -2,10 +2,28 @@
 # Alex Barajas | Alondra Lona
 import glob
 import os
+import numpy
+
+def task1a(c, v, w):
+
+    # initalize table with buffer
+    # table = numpy.zeros(c, len(v))
+    table = [[0] * (c+1) for i in range(len(v) + 1)]
+
+    sizeofv= len(v)
+    for i in range(1, sizeofv + 1):
+        a = i
+        for j in range(1, c + 1):
+            if j - w[i - 1] >= 0:
+                table[i][j] = max(table[i-1][j], v[i-1] + table[i-1][j - w[i-1]])
+            else:
+                table[i][j] = table[i - 1][j]
+    return table[sizeofv][c]
+
+def task1b():
 
 
 def main():
-    print(" Project 3 415 :^)")
     for i in range(1):
         filec = []
         filev = []
@@ -30,6 +48,9 @@ def main():
                 filew = list(map(int, filew))
                 print(filew)
 
+
+
         #### our code here #####
+        print(task1a(filec[0], filev, filew))
 
 main()
